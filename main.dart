8e9668de45b018a4a4827247a7a9d5083113faab
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-04 11:38:42 +0100

    inkl. Niederschlag & Wind (B)

--- main.dart
+++ main.dart
@@ -16,5 +16,29 @@ void main() {
   double? temp3 = (temps[2] ?? 0);
 
   double avgTemp = (temp1 + temp2 + temp3) / 3;
-  print("durchschnittestemperatur: $avgTemp");
+  print("Durchschnittestemperatur: $avgTemp");
+
+  List<double?> rains = [0.9, null, 3.8];
+
+  double? rain1 = (rains[0] ?? 0);
+  double? rain2 = (rains[1] ?? 0);
+  double? rain3 = (rains[2] ?? 2);
+
+  double avgRain = (rain1 + rain2 + rain3) / 3;
+  print("Niederschlagsmenge: $avgRain");
+
+  List<double?> winds = [null, 16.8, null];
+
+  double? wind1 = (winds[0] ?? 0);
+  double? wind2 = (winds[1] ?? 0);
+  double? wind3 = (winds[2] ?? 0);
+
+  double? avgWind = (wind1 + wind2 + wind3) / 3;
+  print("Windgeschwindigkeit: $avgWind");
+
+  print("Durchschnittstemperatur: $avgTemp " +
+      "Niederschlagsmenge: $avgRain " +
+      "Windgeschwindigkeit: $avgWind");
 }
+
+// double? avgRain = (((rains[0] ?? 0), (rains[1] ?? 0), (rains[2] ?? 0)) / 3);
